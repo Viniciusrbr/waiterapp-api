dc821061f4e45c3b42e4207c52d61137f57920a5
viniciusrbr <viniciusgoes159@gmail.com>
2025-02-19 18:46:59 -0300

    feat: create and list categories

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -1,3 +1,16 @@
 import { Request, Response } from 'express'
 
-export function createCategory(req: Request, res: Response) {}
+import { Category } from '../../models/Category'
+
+export async function createCategory(req: Request, res: Response) {
+  try {
+    const { icon, name } = req.body
+
+    const category = await Category.create({ icon, name })
+
+    res.status(201).json(category)
+  } catch (error) {
+    console.error('Error: ', error)
+    res.sendStatus(500)
+  }
+}
