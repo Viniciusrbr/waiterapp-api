3b67b8c2fa34151eb3b2e2987a04fd52822c560f
viniciusrbr <viniciusgoes159@gmail.com>
2025-02-19 22:29:33 -0300

    feat: list products

--- src/app/useCases/products/listProducts.ts
+++ src/app/useCases/products/listProducts.ts
@@ -1,3 +1,14 @@
 import { Request, Response } from 'express'
 
-export function listProducts(req: Request, res: Response) {}
+import { Product } from '../../models/Product'
+
+export async function listProducts(req: Request, res: Response) {
+  try {
+    const products = await Product.find()
+
+    res.json(products)
+  } catch (error) {
+    console.log(error)
+    res.status(500)
+  }
+}
