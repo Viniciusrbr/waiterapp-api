8e8e1c879e37faa1bec3c39d34b3ab3c695d29be
viniciusrbr <viniciusgoes159@gmail.com>
2025-03-06 17:13:42 -0300

    feat: add websocket

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
+import http from 'node:http'
 import path from 'node:path'
 
 import express from 'express'
 import mongoose from 'mongoose'
+import { Server } from 'socket.io'
 
 import { router } from './router'
+
+const app = express()
+const server = http.createServer(app)
+export const io = new Server(server)
 
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express()
-
     app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*')
       res.header('Access-Control-Allow-Methods', '*')
@@ -23,7 +27,7 @@ mongoose
     app.use(express.json())
     app.use(router)
 
-    app.listen(3001, () => {
+    server.listen(3001, () => {
       console.log('Server is running on http://localhost:3001')
     })
 
