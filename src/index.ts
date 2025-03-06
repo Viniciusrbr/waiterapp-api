99ff29115e291a448891b64bf2647af38d551ad6
viniciusrbr <viniciusgoes159@gmail.com>
2025-03-06 16:03:20 -0300

    feat: add CORS headers to allow all origins, methods, and headers

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ mongoose
   .then(() => {
     const app = express()
 
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Methods', '*')
+      res.header('Access-Control-Allow-Headers', '*')
+      next()
+    })
     app.use(
       '/uploads',
       express.static(path.resolve(__dirname, '..', 'uploads')),
